3ee5678125d9bd5b1b44c2efea97d72e7faff158
morphy <36381325+morphy@users.noreply.github.com>
2023-12-14 06:01:28 +0100

    frontend login check

--- apps/papuga-frontend/src/App.tsx
+++ apps/papuga-frontend/src/App.tsx
@@ -1,51 +1,15 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
-import ErrorScreen from "./screens/ErrorScreen";
-import Layout from "./components/Layout";
-import HomeScreen from "./screens/HomeScreen";
-import ItemsScreen from "./screens/ItemsScreen";
-import SetsScreen from "./screens/SetsScreen";
-import CarsScreen from "./screens/CarsScreen";
-import InspectionsScreen from "./screens/InspectionsScreen";
-import RentalsScreen from "./screens/RentalsScreen";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <ErrorScreen />,
-    children: [
-      {
-        path: "home",
-        element: <HomeScreen />
-      },
-      {
-        path: "items",
-        element: <ItemsScreen />
-      },
-      {
-        path: "sets",
-        element: <SetsScreen />
-      },
-      {
-        path: "cars",
-        element: <CarsScreen />
-      },
-      {
-        path: "rentals",
-        element: <RentalsScreen />
-      },
-      {
-        path: "inspections",
-        element: <InspectionsScreen />
-      }
-    ]
-  }
-]);
+import { UserContextProvider } from "./UserContext";
+import router from "./router";
 
 const App: React.FC = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <UserContextProvider>
+      <RouterProvider router={router} />
+    </UserContextProvider>
+  );
 };
 
 export default App;
--- apps/papuga-frontend/src/screens/ItemsScreen/index.tsx
+++ apps/papuga-frontend/src/screens/ItemsScreen/index.tsx
@@ -6,28 +6,39 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 
 import Item from "./Item";
+import { Button } from "@mui/material";
 
 const ItemsScreen = () => {
   const [posts, setPosts] = useState<any[]>([]);
+  const [reload, setReload] = useState(1);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
+    fetch("http://localhost:4000/items", { credentials: "include" })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        throw new Error("Response not ok");
+      })
       .then((data) => setPosts(data))
       .catch((error) => console.log(error));
-  }, []);
+  }, [reload]);
 
   return (
     <Container maxWidth="xl">
       <Typography variant="h4" mt={4}>
         Przedmioty
+        <Button onClick={() => setReload((oldReload) => oldReload + 1)}>
+          Reload
+        </Button>
       </Typography>
 
       <Stack spacing={2} sx={{ py: 8 }}>
         {posts.map((post) => (
           <Item
             key={post.id}
-            name={post.title}
+            name={post.name}
             description={post.body}
             img="https://cataas.com/cat"
           />
